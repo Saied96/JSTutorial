f1c90dcae01b1ff72019c1832280c2da8a598991
email <saiddurrahman@gmail.com>
2022-08-11 23:15:46 -0400

    js fundamental

---
+++ script.js
@@ -0,0 +1,16 @@
+let js = 'amazing';
+      //  if (js === 'amazing') alert('Javascript is Fun');
+ //   40+8+23-10;
+   console.log (40+8+23-10);
+   console.log("jonas");
+   console.log(23);
+let firstName="jonas";
+
+   console.log(firstName);
+   console.log(firstName);
+   console.log(firstName);
+   
+   let myFirstJob = "programmer";
+   let myCurrentJob = "qa engineer";
+   console.log(myFirstJob);
+   console.log(myCurrentJob);
